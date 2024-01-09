3d48d10baf167df56a2d9fcd3d0c5ab1196aef7d
MahiuLDiN <mahiuldin95@gmail.com>
2024-01-10 04:07:09 +0500

    changes

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -11,7 +11,7 @@ export default function Hero() {
               <br /> starts with you
             </h1>
             <div className="flex justify-center">
-              <button className="inline-flex text-white bg-primary border-0 py-2 px-6 focus:outline-none hover:bg-secondary rounded text-lg">
+              <button className="inline-flex text-white bg-primary border-0 py-2 px-6 focus:outline-none hover:bg-secondary rounded text-lg justify-center items-center">
                 Get Started
                 <svg
                 fill="none"
